4559dcc57d61f1ab0deb46f5b717b2a2c306ede2
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2020-08-25 11:30:37 +0200

    rename script to connectdb

---
+++ meetup-crawler/scripts/connectdb.sh
@@ -0,0 +1,13 @@
+AWS_REGION=eu-west-3
+SECRET_ID=meetupcrawlerdatabaseSecret-pDbdefjrmmIg
+SECRET_JSON=$(aws secretsmanager --region $AWS_REGION get-secret-value --secret-id $SECRET_ID --query SecretString | sed -e 's/\\"/"/g' | sed -e 's/"{/{/g' | sed -e 's/}"/}/g')
+echo $SECRET_JSON | jq
+
+USERNAME=$(echo $SECRET_JSON | jq -r .username)
+PASSWORD=$(echo $SECRET_JSON | jq -r .password)
+HOST=$(echo $SECRET_JSON | jq -r .host)
+PORT=$(echo $SECRET_JSON | jq -r .port)
+DBNAME=$(echo $SECRET_JSON | jq -r .dbname)
+
+# psql -h $HOST -p $PORT -U $USERNAME -W $PASSWORD
+psql postgresql://$USERNAME:$PASSWORD@$HOST:$PORT/$DBNAME
--- meetup-crawler/scripts/secret.sh
+++
@@ -1,4 +0,0 @@
-AWS_REGION=eu-west-3
-SECRET_ID=meetupcrawlerdatabaseSecret-pDbdefjrmmIg
-SECRET_JSON=$(aws secretsmanager --region $AWS_REGION get-secret-value --secret-id $SECRET_ID --query SecretString | sed -e 's/\\"/"/g' | sed -e 's/"{/{/g' | sed -e 's/}"/}/g')
-echo $SECRET_JSON | jq
